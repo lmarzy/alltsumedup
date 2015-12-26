4e773e9a548b396dff3811250aaab3a5a2c00ea6
Tilly Marsden <tilly@Tillys-MacBook-Pro.local>
2015-12-26 20:42:45 +0000

    Added base setup

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/*
+
+* MASTRER STARTER TEMPLATE
+
+* see package.json for list of gulp plugins
+
+*/
+
+'use strict';
+
+var requireDir = require('require-dir');
+
+requireDir('./gulp/tasks', { recursive: true} );
